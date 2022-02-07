8e05806bc527ae694410e619e90ed919f941e1ad
RooRsmoka <chri.disa96@gmail.com>
2022-02-07 17:15:38 +0100

    development of methods for API calls and popularBox component created

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,71 @@
 <template>
   <div id="app">
-    <head-bar @search="getContent"/>
-    <main-box :movies="movies"/>
+    <head-bar @search="searchContent"/>
+    <popular-box v-if="!flag" :popularMovies="popularMovies" :popularSeries="popularSeries"/>
+    <main-box :searchedMovies="searchedMovies" :searchedSeries="searchedSeries"/>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import HeadBar from './components/HeadBar.vue'
+import PopularBox from './components/PopularBox.vue'
 import MainBox from './components/MainBox.vue'
+
 
 export default {
   name: 'App',
   components: {
     HeadBar,
+    PopularBox,
     MainBox
   },
   data() {
     return {
-      movies: [],
+      api_key: 'e99307154c6dfb0b4750f6603256716d',
+      popularMovies: [],
+      popularSeries: [],
+      searchedMovies: [],
+      searchedSeries: [],
+      flag: false,
     }
   },
-  methods: {
-    getContent(textInput) {
-      axios.get(`https://api.themoviedb.org/3/search/movie?query=${textInput}&api_key=e99307154c6dfb0b4750f6603256716d`).then((response) => {
-        this.movies = response.data.results;
+  mounted() {
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${this.api_key}&page=1`)
+      .then((response) => {
+        this.popularMovies = response.data.results;
       });
+    axios.get(`https://api.themoviedb.org/3/tv/popular?api_key=${this.api_key}&page=1`)
+      .then((response) => {
+        this.popularSeries = response.data.results;
+      });
+  },
+  methods: {
+    searchContent(textInput) {
+      this.getMovies(textInput);
+      this.getSeriesTv(textInput);
+      this.flag = true;
+    },
+    async getMovies(textInput) {
+      this.searchedMovies = await this.apiCall('movie', textInput)
+    },
+
+    async getSeriesTv(textInput) {
+      this.searchedSeries = await this.apiCall('tv', textInput)
+    },
+
+    async apiCall(type, textInput) {
+
+      const params = {
+        query: textInput,
+        api_key: this.api_key
+      }
+      const results = await axios.get(`https://api.themoviedb.org/3/search/${type}`, { params }
+        )
+        .then((response) => {
+          return response.data.results;
+        })
+      return results
     }
   }
 }
---
+++ src/components/PopularBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <main class="container">
+        <h1 class="cards-title">Popular Movies</h1>
+        <div class="cards-container">
+            <movie-card v-for="popularMovie in popularMovies" :key="popularMovie.id" :movie="popularMovie"/>
+        </div>
+        <h1 class="cards-title">Popular Series TV</h1>
+        <div class="cards-container">
+            <serie-card v-for="popularSerie in popularSeries" :key="popularSerie.id" :serie="popularSerie"/>
+        </div>
+    </main>
+</template>
+
+<script>
+import MovieCard from './MovieCard.vue'
+import SerieCard from './SerieCard.vue'
+export default {
+    components: {
+        MovieCard,
+        SerieCard
+    },
+    props: {
+        popularMovies: Array,
+        popularSeries: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/popularBox.scss';
+</style>
