d076bcb54a3e408abd556c60c65bfa171932b935
RooRsmoka <chri.disa96@gmail.com>
2022-02-07 17:11:42 +0100

    added dynamic flags

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="content-card">
-        <div class="movie-poster">
+        <div class="content-poster">
             <img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" :alt="movie.original_title">
         </div>
-        <div class="movie-info">
-            <p><strong>Title:</strong>  {{movie.title}}</p>
+        <div class="content-info">
+            <p>Title: {{movie.title}}</p>
             <p>Original title: {{movie.original_title}}</p>
-            <p>Language: {{movie.original_language}}</p>
+            <p>
+                Language: {{movie.original_language.toUpperCase()}}
+                <img :src="`/flags/${movie.original_language}.png`" alt="">
+            </p>
             <p>Rating: {{movie.vote_average}}</p>
-            <p>Overview: {{movie.overview}}</p>
         </div>
     </div>
 </template>
@@ -16,11 +18,11 @@
 <script>
 export default {
     props: {
-        movie: Object
-    }
+        movie: Object,
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@/style/movieCard.scss';
+@import '@/style/contentCard.scss';
 </style>
